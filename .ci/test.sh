f9ed9db38627760c1f6c386a8deb9ca59be3a71c
Erik Kristensen <erik@erikkristensen.com>
2017-09-24 19:39:17 -0600

    fixing path

--- .ci/test.sh
+++ .ci/test.sh
@@ -6,21 +6,21 @@ if [ "x$TRAVIS_TAG" != "x" ]; then
   echo "Detected Event: Tag"
   echo " > TAG: ${TRAVIS_TAG}"
   echo ""
-  ./scripts/test-all.sh || exit 1
+  ./.ci/test-all.sh || exit 1
   exit 0
 fi
 
 if [ "$TRAVIS_EVENT_TYPE" != "cron" ]; then
   echo "Detected Event: Push or PR"
   echo ""
-  ./scripts/test-changed.sh || exit 1
+  ./.ci/test-changed.sh || exit 1
   exit 0
 fi
 
 if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
   echo "Detected Event: CRON"
   echo ""
-  ./scripts/test-all.sh || exit 1
+  ./.ci/test-all.sh || exit 1
   exit 0
 fi
 
