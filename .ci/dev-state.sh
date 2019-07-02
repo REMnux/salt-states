5c2ca6b7b582c5d18556c05d90b4047ad521c9e7
Erik Kristensen <erik@erikkristensen.com>
2019-07-02 08:07:31 -0500

    parameterize the scripts

--- .ci/dev-state.sh
+++ .ci/dev-state.sh
@@ -2,7 +2,8 @@
 
 set -x
 
+DISTRO=${DISTRO:="bionic"}
 STATE=$1
 
-docker run -it --rm --name="remnux-state-${STATE}" -v `pwd`/remnux:/srv/salt/remnux --cap-add SYS_ADMIN teamdfir/sift-saltstack-tester:bionic \
+docker run -it --rm --name="remnux-state-${STATE}" -v `pwd`/remnux:/srv/salt/remnux --cap-add SYS_ADMIN teamdfir/sift-saltstack-tester:${DISTRO} \
   /bin/bash
--- .ci/test-state.sh
+++ .ci/test-state.sh
@@ -2,7 +2,8 @@
 
 set -x
 
+DISTRO=${DISTRO:="bionic"}
 STATE=$1
 
-docker run -it --rm --name="remnux-state-${STATE}" -v `pwd`/remnux:/srv/salt/remnux --cap-add SYS_ADMIN teamdfir/sift-saltstack-tester:bionic \
+docker run -it --rm --name="remnux-state-${STATE}" -v `pwd`/remnux:/srv/salt/remnux --cap-add SYS_ADMIN teamdfir/sift-saltstack-tester:${DISTRO} \
   salt-call -l debug --local --retcode-passthrough --state-output=mixed state.sls ${STATE} pillar="{remnux_user: root}"
