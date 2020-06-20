dcf864693692b4f71b45d0635f9d992739a076d2
lennyzeltser <lenny@zeltser.com>
2020-06-20 14:09:47 -0400

    Fixed an xhost error in file name. Updated dev-state.sh to use new Docker image

--- .ci/dev-state.sh
+++ .ci/dev-state.sh
@@ -5,5 +5,5 @@ set -x
 DISTRO=${DISTRO:="bionic"}
 STATE=$1
 
-docker run -it --rm --name="remnux-state-${STATE}" -v `pwd`/remnux:/srv/salt/remnux --cap-add SYS_ADMIN teamdfir/sift-saltstack-tester:${DISTRO} \
+docker run -it --rm --name="remnux-state-${STATE}" -v `pwd`/remnux:/srv/salt/remnux --cap-add SYS_ADMIN remnux/saltstack-tester:${DISTRO} \
   /bin/bash
