8b32b6849a318c39e6ca5af2ee1d65c07f0b4afb
lennyzeltser <lenny@zeltser.com>
2023-10-14 14:32:32 -0400

    Adjusted dev-state.sh to use focal as the default DISTRO

--- .ci/dev-state.sh
+++ .ci/dev-state.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-DISTRO=${DISTRO:="bionic"}
+DISTRO=${DISTRO:="focal"}
 STATE=$1
 
 docker run -it --rm --name="remnux-state-${DISTRO}" -v `pwd`/remnux:/srv/salt/remnux --cap-add SYS_ADMIN remnux/saltstack-tester:${DISTRO} \
