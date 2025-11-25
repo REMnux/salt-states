b68e1afd7c2a9d1f5a0775c5c036d8f857d94dc8
lennyzeltser <lenny@zeltser.com>
2025-11-25 13:23:56 -0500

    Updated dev-state.sh to use Noble by default

--- .ci/dev-state.sh
+++ .ci/dev-state.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DISTRO=${DISTRO:="focal"}
+DISTRO=${DISTRO:="noble"}
 STATE=$1
 echo salt-call -l debug --local --retcode-passthrough --state-output=mixed state.sls remnux.${STATE}
-docker run -it --rm --name="remnux-state-${DISTRO}" -v `pwd`/remnux:/srv/salt/remnux --cap-add SYS_ADMIN ghcr.io/ekristen/cast-tools/saltstack-tester:${DISTRO}-3006 \
+docker run -it --rm --name="remnux-state-${DISTRO}" -v `pwd`/remnux:/srv/salt/remnux --cap-add SYS_ADMIN remnux/saltstack-tester:${DISTRO}3006 \
   /bin/bash
