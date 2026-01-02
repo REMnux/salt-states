b6e9bb81bc46f3aa4ebfea638dce7370dc544dd3
lennyzeltser <lenny@zeltser.com>
2026-01-02 17:00:33 -0500

    Disable checkpoint during cast release in release.sh

--- .ci/release.sh
+++ .ci/release.sh
@@ -215,7 +215,7 @@ echo_success "Tag $TAG pushed to origin"
 
 # Step 8: Run cast release
 echo_info "==> Running cast release..."
-cast release --rm-dist
+CHECKPOINT_DISABLE=1 cast release --rm-dist
 
 echo_success "Cast release completed"
 
